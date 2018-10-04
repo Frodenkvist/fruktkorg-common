9de2a6f5c216469c4b855fe465bd16f70adc0458
Mio Engel <mioengel@gmail.com>
2018-10-04 10:08:38 +0200

    Inital commit

---
+++ src/main/java/com/common/rabbitmq/RabbitMQRouting.java
@@ -0,0 +1,13 @@
+package com.common.rabbitmq;
+
+public class RabbitMQRouting {
+    private RabbitMQRouting() {}
+
+    public enum Exchange {
+        PERSON
+    }
+
+    public enum Person {
+        CREATE
+    }
+}
